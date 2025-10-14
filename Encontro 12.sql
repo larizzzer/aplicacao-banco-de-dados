fe7618bf82b8820e929c9471a38d0903ae90414e
Larissa Gomes <larissagomesgaspar@gmail.com>
2025-10-14 11:25:59 -0300

    Update em Encontro 12
    Alguns exemplos de Funções que podem ser usadas em SQL no SELECT.

--- Encontro 12.sql
+++ Encontro 12.sql
@@ -19,3 +19,26 @@ UPDATE E12_Produtos SET Estoque = 400 WHERE Id = 4;
 DELETE FROM E12_Produtos WHERE Id = 3;
 
 SELECT * FROM E12_Produtos;
+
+SELECT 
+	Nome, LENGTH(Nome) AS Tamanho, -- Retorna o comprimento do campo
+    UPPER(Nome) AS Maiusculas, -- Deixa todas as letras maiúsculas
+    LOWER(Nome) AS Minusculas -- Deixas todas as letras minúsculas
+FROM E12_Produtos;
+
+SELECT
+	Preco,
+    ROUND(Preco,0) AS 'Preco_Arredondado', -- Arredonda o valor para quantas casas decimais quiser
+    ABS(Preco), -- Valor sempre positivo (absoluto)
+    CEILING(Preco), -- Arredonda pra cima sempre
+    FLOOR(Preco), -- Arredonda pra baixo sempre
+    ROUND(Preco / 2,2) AS '50% de Desconto', -- Exemplo de calcúlo em uma coluna
+    ROUND(Preco * 0.9,2) AS '10% de Desconto' -- aplica 10% no valor
+FROM E12_Produtos;
+
+SELECT 
+	COUNT(*) AS 'Quantidade de Registros', -- Retorna a quantidade de registros dessa coluna
+    AVG(Preco) AS 'Média de preço', -- Retorna a média aritmética da coluna
+    Nome
+FROM E12_Produtos 
+GROUP BY Nome; -- Group By para agrupar resultados em uma coluna específica. É obrigatório em leituras que utilizam funções de agregação aliadas a colunas comuns
